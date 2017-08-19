072db3042d6a6f3f12aa6f56895d0912f5410a08
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-08-18 23:12:25 -0700

    Add a check for the requited parameters

--- go.sh
+++ go.sh
@@ -12,11 +12,18 @@
 # machine
 
 lx1_list=
+lx1_set=false
 ly1_list=
 lz1_list=
+
 lelt_list=
+lelt_set=false
+
 lp_list=
+lp_set=false
+
 machine_list=
+machine_set=false
 
 this_file="${BASH_SOURCE[0]}"
 
@@ -26,6 +33,7 @@ while [ $# -gt 0 ]; do
          -x|--lx1)
            shift
            lx1_list=$1
+           lx1_set=true
            ;;
          -y|--ly1)
            shift
@@ -38,18 +46,40 @@ while [ $# -gt 0 ]; do
          -e|--lelt)
            shift
            lelt_list=$1
+           lelt_set=true
            ;;
          -p|--lp)
            shift
            lp_list=$1
+           lp_set=true
            ;;
          -m|--machine)
            shift
            machine_list=$1
+           machine_set=true
            ;;
   esac
   shift
 done # end reading arguments
+
+# Check if the requited variables are set
+echo "$lx1_set"
+echo "$lelt_set"
+echo "$lp_set"
+echo "$machine_set"
+
+if [ ${lx1_set} = false ] || [ ${lelt_set} = false ] \
+      || [ ${lp_set} = false ] || [ ${machine_set} = false ]; then
+  echo "You need to specify at least lx1, lelt, lp and machine parameters"
+fi
+
+# Set ly1 and lz1 to lx1 by default if not specified
+if [ ${#ly1_list} -eq 0 ]; then
+  ly1_list=$lx1_list
+fi
+if [ ${#lz1_list} -eq 0 ]; then
+  lz1_list=$lx1_list
+fi
 
 echo "lx1 = $lx1_list"
 echo "ly1 = $ly1_list"
