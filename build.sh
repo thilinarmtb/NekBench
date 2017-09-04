f057f43b4ea4ef40255f50683c5ca2d6da405d2b
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-09-04 17:01:15 -0500

    Add the build script

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 size=${#test_list[@]}
 size=$(( size - 1 ))
-echo $size
 
 first_test=${test_list[0]}
 cd cases/$case_basename/$first_test
@@ -11,8 +10,17 @@ for lelt in $lelt_list; do
    mkdir lelt_"${lelt}"
    cd lelt_"${lelt}"
    for lx1 in $lx1_list; do
-      mkdir lx_"${lx1}" 
-      cd lx_"${lx1}" 
+      mkdir lx_"${lx1}"
+      cd lx_"${lx1}"
+        cp -r $case .
+        cd $case_basename
+          local lxd=$(( 3*lx1/2 ))
+          sed "s/lx1=[0-9]*/lx1=${lx1}/" SIZE > SIZE.1
+          sed "s/lxd=[0-9]*/lxd=${lxd},/" SIZE.1 > SIZE.2
+          sed "s/lelt=[0-9]*/lelt=${lelt},/" SIZE.2 > SIZE.3
+          rm SIZE.1 SIZE.2
+          mv SIZE.3 SIZE
+        cd ..
       cd ..
    done
    cd ..
@@ -21,6 +29,5 @@ done
 cd ..
 
 for i in `seq 1 1 $size`; do
-  echo $i
+   cp -r $first_test ${test_list[i]}
 done
-
