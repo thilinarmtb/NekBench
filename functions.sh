52be6a64cdb682e431e9841642545d7b500cbfd1
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-09-05 22:23:43 -0500

    Improve the functions

--- functions.sh
+++ functions.sh
@@ -10,6 +10,18 @@ function abs() {
   echo $in
 }
 
+function test_abs() {
+  local rslt1=$(abs -5)
+  local rslt2=$(abs 5)
+
+  if [ $rslt1 -eq 5 ] && [ $rslt2 -eq 5 ]; then
+    echo "abs: Passed. Result=${rslt1}"
+  else
+    echo "abs: Failed. Result=${rslt1}"
+  fi
+}
+test_abs
+
 function max() {
   local arg_list=("${@}")
   local max=0
@@ -25,12 +37,39 @@ function max() {
 
 function test_max()
 {
-  local a=(-1 0 2 -3 5 2 -7)
+  local a=(1 2 3 7 2 5)
   local rslt=$(max "${a[@]}")
 
-  if [ $rslt = 5 ]; then
+  if [ $rslt -eq 7 ]; then
     echo "max: Passed. Result=${rslt}"
   else
     echo "max: Failed. Result=${rslt}"
   fi 
 }
+test_max
+
+function min() {
+  local arg_list=("${@}")
+  local min="${arg_list[0]}"
+
+  for elem in "${arg_list[@]}"; do
+    if [ $elem -lt $min ]; then
+      min=$elem
+    fi
+  done
+
+  echo $min
+}
+
+function test_min()
+{
+  local a=(2 3 5 2 7 1)
+  local rslt=$(min "${a[@]}")
+
+  if [ $rslt -eq 1 ]; then
+    echo "min: Passed. Result=${rslt}"
+  else
+    echo "min: Failed. Result=${rslt}"
+  fi
+}
+test_min
