b5b8020308148ff6a5be64c3a0635919ee940559
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-09-07 20:43:48 -0500

    Polishing the scripts

--- scaling.sh
+++ scaling.sh
@@ -2,7 +2,6 @@
 
 echo " ================ Doing Scaling Test ================ "
 echo " case name: ${case_basename}"
-echo " ==================================================== "
 
 cd $RUNS_DIR/$case_basename/scaling
 
@@ -12,17 +11,21 @@ for lelt in $lelt_list; do
     cd lx_"${lx1}"
       cd $case_basename
         # Build the case
+        echo "Building lelt=${lelt}, lx1=${lx1}"
         cp $BASE_DIR/makeneks/makenek.${machine} .
-        ./makenek.${machine} $case_basename
+        ./makenek.${machine} $case_basename > buildlog
 
         # Do the scaling test
         for np in $np_list; do
-          $JOBS_DIR/submit.${machine} ${case_basename} ${np}
+          echo "Running the case with np=${np}"
+          $JOBS_DIR/submit.${machine} ${case_basename} ${np} scaling
         done
       cd ..
     cd ..
   done
   cd ..
 done
+
+echo " ==================================================== "
 
 cd $BASE_DIR
