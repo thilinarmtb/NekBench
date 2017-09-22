d3a364abe9a110df5571a4cb6d93bf3f5debd66b
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-09-22 08:37:23 +0000

    Fix a bug with machine files

--- scaling.sh
+++ scaling.sh
@@ -4,6 +4,8 @@ echo "Nek5000        : ${NB_NEK5_DIR}"
 echo "makenek script : ${NB_MKNK_DIR}/makenek.${nb_machine}"
 echo "submit script  : ${NB_JOBS_DIR}/submit.${nb_machine}"
 
+# Get rid of this export and matching unset
+export NB_NEK5_DIR
 
 cd $NB_RUNS_DIR/$nb_case_basename/scaling
 
@@ -26,8 +28,7 @@ for lelt in $nb_lelt_list; do
         for nb_np in $nb_np_list; do
           . ${NB_MCHN_DIR}/${nb_machine}
           echo "    Running the case with np=${nb_np} ..."
-          ${NB_RUN_CMD} ${NB_JOBS_DIR}/submit.${nb_machine} \
-                 ${nb_case_basename} scaling ${nb_np} ${nb_ppn}
+          ${NB_RUN_CMD} ${NB_JOBS_DIR}/submit.${nb_machine} ${nb_case_basename} scaling ${nb_np} ${nb_ppn}
         done
       cd ..
     cd ..
@@ -38,3 +39,5 @@ done
 echo "==================================================== "
 
 cd $NB_BASE_DIR
+
+unset NB_NEK5_DIR
