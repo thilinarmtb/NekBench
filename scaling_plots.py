c592ec05ed2351bc8edc500e2d94e59b47929640
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-11-11 22:06:17 -0600

    Start editing the plotting scripts

--- scaling_plots.py
+++ scaling_plots.py
@@ -26,43 +26,33 @@ datafile =  args.datafile
 
 #read data
 for t in tag:
-    data = readfile(os.path.join(directory, t, datafile))
-    print(data)
+    time_data = readfile(os.path.join(directory, t, datafile))
 
-## Anatomy of a figure: https://matplotlib.org/faq/usage_faq.html
-fig          = plt.figure()
-fig, ax_list = plt.subplots(1, 1)
+    # set machine, lelt, lx and case arrays
 
-ax_list = [ax_list]
-ax_list[0].set_xlabel("Number of MPI ranks")
-ax_list[0].set_ylabel("Time (s)")
-fig.suptitle("Scaling study")
+    ## Anatomy of a figure: https://matplotlib.org/faq/usage_faq.html
+    fig          = plt.figure()
+    fig, ax_list = plt.subplots(1, 1)
 
-#for m in machine:
-#    for t in tag:
-#        for e in lelt:
-#            for x in lx:
-#                f = directory + "/" + m       +      \
-#                                "/" + t +            \
-#                                "/lelt_" + str(e) +  \
-#                                "/lx_" + str(x) +    \
-#                                "/" + case[0] +         \
-#                                "/" + data_file
-#
-#                time_data    = readfile(f)
-##                print(time_data)
-##                print(nprocs)
-#                perfect_data = [time_data[0]/p for p in nprocs]
-#
-#                label = 'machine = ' + m + 'tag = ' + t + \
-#                              ', lelt = ' + str(e) + ', lx = ' + str(x)
-#                ax_list[0].loglog(nprocs, time_data   , '-o', label = label)
-#
-#                label = 'perfect, ' + label
-#                ax_list[0].loglog(nprocs, perfect_data, '--', label = label)
-#
-#ax_list[0].legend()
-#
-#pdfname = case[0] + "_" + tag[0] + "_scaling.pdf"
-#fig.savefig(pdfname)
-#print("Scaling figure saved in: " + pdfname)
+    ax_list = [ax_list]
+    ax_list[0].set_xlabel("Number of MPI ranks")
+    ax_list[0].set_ylabel("Time (s)")
+    fig.suptitle("Scaling study")
+
+    for m in machine:
+        for e in lelt:
+            for x in lx:
+                perfect_data = [time_data[0]/p for p in nprocs]
+
+                label = 'machine = ' + m + 'tag = ' + t + \
+                              ', lelt = ' + str(e) + ', lx = ' + str(x)
+                ax_list[0].loglog(nprocs, time_data   , '-o', label = label)
+
+                label = 'perfect, ' + label
+                ax_list[0].loglog(nprocs, perfect_data, '--', label = label)
+
+            ax_list[0].legend()
+
+    pdfname = case + "_" + tag + "_scaling.pdf"
+    fig.savefig(pdfname)
+    print("Scaling figure saved in: " + pdfname)
