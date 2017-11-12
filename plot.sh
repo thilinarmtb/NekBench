55eac7cae3adf21026a71738f13cf23046890c5c
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-11-11 22:00:51 -0600

    Read the data from file

--- plot.sh
+++ plot.sh
@@ -21,9 +21,11 @@ for tag in ${nb_tag_list[@]}; do
 
   for machine in ${nb_machine_list[@]}; do
     # Find tag directory
+    # TODO Change this to a function
     nb_plt_tagdir="$NB_BENCH_DIR/${tag}/${machine}"
 
     # Read metadata from README file in the tag directory
+    # TODO Change this to a function
     nb_plt_readme="${nb_plt_tagdir}/$NB_README_FILE"
     readarray a < "${nb_plt_readme}"
 
@@ -51,24 +53,35 @@ for tag in ${nb_tag_list[@]}; do
       esac
     done
 
+    # Convert data to bash arrays
+    nb_plt_lx1=(${nb_plt_lx1[@]})
+    nb_plt_lelt=(${nb_plt_lelt[@]})
+    nb_plt_case=(${nb_plt_case[@]})
+    nb_plt_np=(${nb_plt_np[@]})
+    nb_plt_ppn=(${nb_plt_ppn[@]})
+
     # Read data from log files
-    for lelt in ${nb_plt_lelt[@]}; do
-      for lx in ${nb_plt_lx1[@]}; do
-        # TODO: Turn these into functions
-        nb_plt_logprfx="${nb_plt_case}.log.${nb_tag}."
-        nb_plt_logpath="${nb_plt_tagdir}/lelt_${lelt}/lx_${lx}/${nb_plt_case}/"
-        cd ${nb_plt_logpath}
+    for cse in ${nb_plt_case[@]}; do
+      for lelt in ${nb_plt_lelt[@]}; do
+        for lx in ${nb_plt_lx1[@]}; do
+          # TODO: Turn these into functions
+          nb_plt_logprfx="${nb_plt_case}.log.${nb_tag}."
+          nb_plt_logpath="${nb_plt_tagdir}/lelt_${lelt}/lx_${lx}/${nb_plt_case}/"
+          cd ${nb_plt_logpath}
 
-          for index in ${!nb_plt_np[@]}; do
-            np=(${nb_plt_np[$index]})
-            ppn=(${nb_plt_ppn[$index]})
-            # TODO Change this to a function
-            logfile="${nb_plt_logprfx}${np}.${ppn}"
+            for index in ${!nb_plt_np[@]}; do
+              np=${nb_plt_np[${index}]}
+              ppn=${nb_plt_ppn[${index}]}
 
-            tmp=($(grep "total solver time" ${logfile}*))
-            echo "$tag $machine $lelt $lx $case ${tmp[6]}" >> ${NB_BENCH_DIR}/${tag}/${NB_TIME_DATA}
-          done
-        cd - > /dev/null
+              # TODO Change this to a function
+              logfile="${nb_plt_logprfx}${np}.${ppn}"
+              tmp=($(grep "total solver time" ${logfile}*))
+              # TODO Change this to a function
+              echo "$tag $machine $lelt $lx $cse $np $ppn ${tmp[6]}" >> \
+                       ${NB_BENCH_DIR}/${tag}/${NB_TIME_DATA}
+            done
+          cd - > /dev/null
+        done
       done
     done
   done
@@ -77,7 +90,7 @@ done
 #-----------------------------------------------------------------------
 # Print debug information
 #-----------------------------------------------------------------------
-nb_debug_scripts=true
+nb_debug_scripts=false
 if [ ${nb_debug_scripts} = true ]; then
   echo "${nb_plt_lx1[@]}"
   echo "${nb_plt_lelt[@]}"
@@ -89,13 +102,8 @@ fi
 #-----------------------------------------------------------------------
 # Plot data
 #-----------------------------------------------------------------------
-if [ $nb_plt_type = "scaling" ]; then
-  python scaling_plots.py --lx        ${nb_plt_lx1[@]}   \
-                          --lelt      ${nb_plt_lelt[@]}  \
-                          --np        ${nb_plt_np[@]}    \
-                          --machine   ${nb_machine_list[@]}   \
-                          --tag       ${nb_tag[@]}       \
-                          --case      ${nb_plt_case[@]}  \
+if [ ${nb_plt_type} = "scaling" ]; then
+  python scaling_plots.py --tag       ${nb_tag[@]}    \
                           --directory ${NB_BENCH_DIR} \
-                          --data_file ${NB_TIME_DATA}
+                          --datafile ${NB_TIME_DATA}
 fi
--- scaling_plots.py
+++ scaling_plots.py
@@ -1,43 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import os
 
 def readfile(f):
-    time = []
-#    print(f)
+    data = []
     with open(f, "r") as timedata:
         for t in timedata:
-            time.append(float(t.split()[0]))
-    return np.array(time)
+            data.append(t.split())
+    return data
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-x"  , "--lx"  , nargs="+",                 \
-                         help = "lx1 value lists"    , required = True)
-parser.add_argument("-e"  , "--lelt", nargs="+",      type = int,\
-                         help = "lelt value lists"   , required = True)
-parser.add_argument("-n"  , "--np"  , nargs="+",      type = int,\
-                         help = "np value list"      , required = True)
-parser.add_argument("-m"  , "--machine" , nargs="+",             \
-                         help = "Machine name"       , required = True)
-parser.add_argument("-t"  , "--tag" , nargs="+",                 \
+parser.add_argument("-t", "--tag" , nargs="+",                 \
                          help = "Tag value"          , required = True)
-parser.add_argument("-c"  , "--case", nargs="+",      type = str,\
-                         help = "Case name"          , required = True)
-parser.add_argument("-dir", "--directory", nargs="?",            \
+parser.add_argument("-d", "--directory", nargs="?",            \
                          help = "Benchmark directory", required = True)
-parser.add_argument("-df" , "--data_file", nargs="?", type = str,\
+parser.add_argument("-f", "--datafile", nargs="?", type = str,\
                          help = "Data file to read"  , required = True)
 
-args = parser.parse_args()
-
-lx        = args.lx
-lelt      = args.lelt
-nprocs    = args.np
-machine   = args.machine
-tag       = args.tag
-case      = args.case
+# Parse the arguements
+args =      parser.parse_args()
 directory = args.directory
-data_file = args.data_file
+tag =       args.tag
+datafile =  args.datafile
+
+#read data
+for t in tag:
+    data = readfile(os.path.join(directory, t, datafile))
+    print(data)
 
 ## Anatomy of a figure: https://matplotlib.org/faq/usage_faq.html
 fig          = plt.figure()
@@ -48,31 +38,31 @@ ax_list[0].set_xlabel("Number of MPI ranks")
 ax_list[0].set_ylabel("Time (s)")
 fig.suptitle("Scaling study")
 
-for m in machine:
-    for t in tag:
-        for e in lelt:
-            for x in lx:
-                f = directory + "/" + m       +      \
-                                "/" + t +            \
-                                "/lelt_" + str(e) +  \
-                                "/lx_" + str(x) +    \
-                                "/" + case[0] +         \
-                                "/" + data_file
-
-                time_data    = readfile(f)
-#                print(time_data)
-#                print(nprocs)
-                perfect_data = [time_data[0]/p for p in nprocs]
-
-                label = 'machine = ' + m + 'tag = ' + t + \
-                              ', lelt = ' + str(e) + ', lx = ' + str(x)
-                ax_list[0].loglog(nprocs, time_data   , '-o', label = label)
-
-                label = 'perfect, ' + label
-                ax_list[0].loglog(nprocs, perfect_data, '--', label = label)
-
-ax_list[0].legend()
-
-pdfname = case[0] + "_" + tag[0] + "_scaling.pdf"
-fig.savefig(pdfname)
-print("Scaling figure saved in: " + pdfname)
+#for m in machine:
+#    for t in tag:
+#        for e in lelt:
+#            for x in lx:
+#                f = directory + "/" + m       +      \
+#                                "/" + t +            \
+#                                "/lelt_" + str(e) +  \
+#                                "/lx_" + str(x) +    \
+#                                "/" + case[0] +         \
+#                                "/" + data_file
+#
+#                time_data    = readfile(f)
+##                print(time_data)
+##                print(nprocs)
+#                perfect_data = [time_data[0]/p for p in nprocs]
+#
+#                label = 'machine = ' + m + 'tag = ' + t + \
+#                              ', lelt = ' + str(e) + ', lx = ' + str(x)
+#                ax_list[0].loglog(nprocs, time_data   , '-o', label = label)
+#
+#                label = 'perfect, ' + label
+#                ax_list[0].loglog(nprocs, perfect_data, '--', label = label)
+#
+#ax_list[0].legend()
+#
+#pdfname = case[0] + "_" + tag[0] + "_scaling.pdf"
+#fig.savefig(pdfname)
+#print("Scaling figure saved in: " + pdfname)
