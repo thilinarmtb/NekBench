17e5be522528bcc6183878ff64695076d1b6f5bb
Thilina Rathnayake <thilinarmtb@gmail.com>
2018-07-30 20:48:44 -0500

    Polishing

--- build.sh
+++ build.sh
@@ -25,8 +25,6 @@ for i in ${!NB_PAR[@]}; do
 for j in ${!NB_PAR[@]}; do
   par_i=(${NB_PAR[$i]})
   par_j=(${NB_PAR[$j]})
-# echo "par_i = ${par_i[@]}"
-# echo "par_j = ${par_j[@]}"
   if [ "$i" -ne "$j" ] && [ $i -lt $j ]; then
     nb_par_i_id=${par_i[0]}
     nb_par_i_vals=(${par_i[@]:1})
@@ -35,9 +33,6 @@ for j in ${!NB_PAR[@]}; do
     for i_val in ${nb_par_i_vals[@]}; do
     for j_val in ${nb_par_j_vals[@]}; do
     (( dbg_count += 1 ))
-#     echo "ijpar ${nb_par_i_id} ${nb_par_j_id}"
-#     echo "ijval ${i_val} ${j_val}"
-#     echo "ij $i $j"
       dir="p_${nb_par_i_id}_${i_val}_${nb_par_j_id}_${j_val}"
       mkdir "${dir}"
       cd "${dir}"
